812f60a01a727000d44d7ce25764b601fb34b05b
William Gardner <wg4568@gmail.com>
2020-01-07 13:19:11 -0800

    randomization server side

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__, template_folder='pages/')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    title = '/static/img/title/title (%s).jpg' % random.randint(1, 4)
+    home = '/static/img/home/home (%s).jpg' % random.randint(1, 31)
+    return render_template('index.html', title=title, home=home)
 
 app.run(host='0.0.0.0', port=8000)
