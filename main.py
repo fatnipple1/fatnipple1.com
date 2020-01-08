dff9d6cc4cd0743051fb254b6b8617893be13734
William Gardner <539362@cuhsd-int.prv>
2020-01-08 13:03:48 -0800

    ten minute title

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_file, abort, session
 from lib import security
 import random
 import os
+import time
 
 app = Flask(__name__, template_folder='pages/')
 app.secret_key = security.app_secret_key()
@@ -11,6 +12,13 @@ for root, dirs, files in os.walk('img/'):
     norm = lambda f: os.path.normpath(root + '/' + f).replace('\\', '/')
     images.extend(norm(f) for f in files)
 
+@app.before_request
+def before_request():
+    if 'title' not in session or session['expires'] < time.time():
+        title = 'img/title/title (%s).jpg' % random.randint(1, len(os.listdir('img/title/')))
+        session['title'] = title
+        session['expires'] = time.time() + 600 # ten minutes
+
 @app.route('/img/<path:img>')
 def image(img):
     filepath = 'img/' + img
@@ -19,10 +27,8 @@ def image(img):
 
 @app.route('/')
 def welcome():
-    title = 'img/title/title (%s).jpg' % random.randint(1, len(os.listdir('img/title/')))
     home = 'img/home/home (%s).jpg' % random.randint(1, len(os.listdir('img/home/')))
-    session['title'] = title
-    return render_template('entrance.html', title=title, home=home)
+    return render_template('entrance.html', title=session['title'], home=home)
 
 @app.route('/fatnipple1.html')
 def fatnipple1():
